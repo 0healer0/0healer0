c832d78935c7059241b3aec0b9f2a14ac4332bed
Healer <hienthulinhnhi@gmail.com>
2021-12-09 22:40:39 +0700

    bai 16

---
+++ lesson16/app.js
@@ -0,0 +1,36 @@
+function loadTexture(path) {
+  return new Promise((resolve) => {
+    const img = new Image()
+    img.src = path
+    img.onload = () => {
+      resolve(img)
+    }
+  })
+}
+
+function createEnemies(ctx, canvas, monsterImg) {
+  const MONSTER_TOTAL = 6;
+  const MONSTER_WIDTH = MONSTER_TOTAL * 98;
+  const START_X = (canvas.width - MONSTER_WIDTH) / 2;
+  const STOP_X = START_X + MONSTER_WIDTH;
+  
+  for (let x = START_X; x < STOP_X; x += 98) {
+    for (let y = 0; y < 50 * 5; y += 50) {
+      ctx.drawImage(monsterImg, x, y);
+    }
+  }
+}
+
+window.onload = async () => {
+  canvas = document.getElementById('canvas')
+  ctx = canvas.getContext('2d');
+    const heroImg = await loadTexture('assets/enemyShip.png')
+    const monsterImg = await loadTexture('assets/player.png')
+    
+   
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0,0, 1024, 768);
+    ctx.drawImage(heroImg, canvas.width/2 - 45, canvas.height - (canvas.height /4));
+
+    createEnemies(ctx, canvas, monsterImg);
+};
