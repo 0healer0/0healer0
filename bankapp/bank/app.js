012fc8d173837fac27e0a98db3645a9c860c1511
Healer <hienthulinhnhi@gmail.com>
2021-12-31 23:00:54 +0700

    bai 23

--- bankapp/bank/app.js
+++ bankapp/bank/app.js
@@ -1,7 +1,8 @@
+let account = null;
 const routes = {
-    '/login': { templateId: 'login' },
-    '/dashboard': { templateId: 'dashboard' },
-  };
+  '/login': { templateId: 'login' },
+  '/dashboard': { templateId: 'dashboard', init: updateDashboard }
+};
   function updateRoute() {
         const path = window.location.pathname;
         const route = routes[path];
@@ -14,6 +15,9 @@ const routes = {
     const app = document.getElementById('app');
     app.innerHTML = '';
     app.appendChild(view);
+    if (typeof route.init === 'function') {
+      route.init();
+    }
   }
   function navigate(path) {
     window.history.pushState({}, path, path);
@@ -37,8 +41,12 @@ async function register() {
   if (result.error) {
     return console.log('An error occurred:', result.error);
   }
-
+  else{
+  alert ("!!!Sign Up Success!!!")
   console.log('Account created!', result);
+  }
+  account = result;
+  navigate('/dashboard');
 }
 
 async function createAccount(account) {
@@ -52,4 +60,62 @@ async function createAccount(account) {
   } catch (error) {
     return { error: error.message || 'Unknown error' };
   }
+}
+
+async function login() {
+  const loginForm = document.getElementById('loginForm')
+  const user = loginForm.user.value;
+  const data = await getAccount(user);
+
+  if (data.error) {
+    return console.log('loginError', data.error);
+  }
+
+  account = data;
+  navigate('/dashboard');
+
+  if (data.error) {
+    return updateElement('loginError', data.error);
+  }
+}
+
+async function getAccount(user) {
+  try {
+    const response = await fetch('//localhost:5000/api/accounts/' + encodeURIComponent(user));
+    return await response.json();
+  } catch (error) {
+    return { error: error.message || 'Unknown error' };
+  }
+}
+
+function updateElement(id, textOrNode) {
+  const element = document.getElementById(id);
+  element.textContent = ''; // Removes all children
+  element.append(textOrNode);
+}
+
+function updateDashboard() {
+  if (!account) {
+    return navigate('/login');
+  }
+
+  updateElement('description', account.description);
+  updateElement('balance', account.balance.toFixed(2));
+  updateElement('currency', account.currency);
+  const transactionsRows = document.createDocumentFragment();
+for (const transaction of account.transactions) {
+  const transactionRow = createTransactionRow(transaction);
+  transactionsRows.appendChild(transactionRow);
+}
+updateElement('transactions', transactionsRows);
+}
+
+function createTransactionRow(transaction) {
+  const template = document.getElementById('transaction');
+  const transactionRow = template.content.cloneNode(true);
+  const tr = transactionRow.querySelector('tr');
+  tr.children[0].textContent = transaction.date;
+  tr.children[1].textContent = transaction.object;
+  tr.children[2].textContent = transaction.amount.toFixed(2);
+  return transactionRow;
 }
